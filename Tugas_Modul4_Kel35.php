7608b57c8a7b5342ca14aae7a9f29b63b06e89ce
MuhammadSulthonAuliya <82279613+MuhammadSulthonAuliya@users.noreply.github.com>
2021-04-18 06:11:02 +0700

    Update and rename Test.txt to Tugas_Modul4_Kel35.php

---
+++ Tugas_Modul4_Kel35.php
@@ -0,0 +1,30 @@
+#method
+class jual_beli:
+	def harga_total(jumlah_barang, harga_satuan):
+		return harga_total=jumlah_barang*harga_satuan
+#function
+def identitas_kelompok():
+	print("Kelompok 35)
+	print("Shift 2")
+	print("Engelbert Jubile Satrio Lukito   21120120140080")
+	print("Muhammad Sulthon Auliya          21120120130047")
+	print("Rafif Sadid Hamdani              21120120140137")
+
+identitas_kelompok()
+
+#while
+while(.....):
+	#switch case python berupa array
+	produk={
+		1:"Buku Tulis",
+		2:"Pensil 2B",
+		3:"..."
+		}
+	x = int(input("Masukan barang yang anda inginkan berdasarkan angka(1-.....): "))
+	print('Hari: %s'%produk.setdefault(x,'Inputan hanya 1-....'))
+
+	#operasi class jual_beli
+	.....
+
+
+harga_semua
