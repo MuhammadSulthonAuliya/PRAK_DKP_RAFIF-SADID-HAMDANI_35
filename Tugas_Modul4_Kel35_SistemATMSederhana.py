b4ca32d21eb7bc91b3cd61c5050ad4d170407753
MuhammadSulthonAuliya <82279613+MuhammadSulthonAuliya@users.noreply.github.com>
2021-04-18 16:37:35 +0700

    Update Tugas_Modul4_Kel35_SistemATMSederhana.py

--- Tugas_Modul4_Kel35_SistemATMSederhana.py
+++ Tugas_Modul4_Kel35_SistemATMSederhana.py
@@ -1,18 +1,35 @@
-class
-
+class jual_beli:
   #fungsi rumus harga kali banyak barang
-  def harga_total():
-  ......
+  def harga_total(int harga_satuan, int jumlah_barang):
+    return harga_satuan*jumlah_barang
   
  #fungsi tanpa kembalian berisi identitas kelompok 
-  def ......
-  print("Kelompok 35")
-  print("Shift 2")
-  print("Eng....")
-  print("Muhammad Sulthon Auliya 21120120130047")
-  print("Rafif ....")
+  def Identitas_kelompok():
+    print("Kelompok 35")
+    print("Shift 2")
+    print("Eng....")
+    print("Muhammad Sulthon Auliya 21120120130047")
+    print("Rafif ....")
+  
+#program main/untuk eksekusi
+
   
   #perulangan berupa jumlah berapa kali transksi
-  
+while(x=="y"):
     #switch case pada python menggunakan array. sebagai datalist barang yang tersedia untuk pemilihan barang.
+  list_barang:{
+    1:"Buku tulis",
+    2:"Pensil",
+    3:"Bolpoin"
+    } 
+  pilihan=int(input("masukan pilihan anda"))
+  print("Barang yang tersedia: %s"%list_barang.set.default(pilihan, "pilih (1-3)"))
+  
+  
+  
     
+  x=input("Beli barang lain?("y"=yes,selain itu dianggap no) ")   
+  
+  
+#penjelasan
+Program ini merupakan program sederhana yang berfungsi melakukan mekanisme perhitungan jual beli.
