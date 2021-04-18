ed0a3c18dfa2079f61e517b8691e64cd514042ca
MuhammadSulthonAuliya <82279613+MuhammadSulthonAuliya@users.noreply.github.com>
2021-04-18 15:17:47 +0700

    Update and rename Test.txt to Tugas_Modul4_Kel35_SistemATMSederhana.py

---
+++ Tugas_Modul4_Kel35_SistemATMSederhana.py
@@ -0,0 +1,18 @@
+class
+
+  #fungsi rumus harga kali banyak barang
+  def harga_total():
+  ......
+  
+ #fungsi tanpa kembalian berisi identitas kelompok 
+  def ......
+  print("Kelompok 35")
+  print("Shift 2")
+  print("Eng....")
+  print("Muhammad Sulthon Auliya 21120120130047")
+  print("Rafif ....")
+  
+  #perulangan berupa jumlah berapa kali transksi
+  
+    #switch case pada python menggunakan array. sebagai datalist barang yang tersedia untuk pemilihan barang.
+    
